815c86f6c4b91c05461a29b285c870b919ae62a4
Gabriel <gabriel.lune@roit.com.br>
2022-05-05 22:19:28 -0300

    chore(env): including database variables

--- src/repositories/config/MySqlConfig.ts
+++ src/repositories/config/MySqlConfig.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize'
 
-export const sequelize = new Sequelize('test', 'root', '160412Timao@', {
-    host: "localhost",
-    dialect: "mysql"
+export const sequelize = new Sequelize(process.env.TON_SITE, process.env.TON_SITE, process.env.TON_SITE, {
+    host: process.env.TON_SITE,
+    dialect: 'mysql'
 })
 export class MySqlConfig {
 

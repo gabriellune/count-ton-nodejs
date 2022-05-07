84837de85b0ae69c7ab8a07e2898b241940cd1e3
Gabriel <gabriel.lune@roit.com.br>
2022-05-07 01:00:21 -0300

    refactor(services): improving flow, validations and returns

--- src/services/CountApiService.ts
+++ src/services/CountApiService.ts
@@ -1,33 +1,38 @@
 import countapi from 'countapi-js';
+import { Request, Response } from 'express';
+import { error, success } from '../interceptors/ResponseInterceptor';
 import { ErrorHandle } from '../interceptors/ErrorHandle';
 import { CountApi } from '../models/CountApi';
 
 export class CountApiService {
 
-    async getCount(): Promise<CountApi> {
+    async getCount(res: Response): Promise<CountApi> {
         try {
             const result = await countapi.get(process.env.TON_SITE, process.env.COUNT_API_KEY)
 
-            return result
+            return res.status(200).send(success('SUCCESS', { data: result }, 200))
 
         } catch (err) {
-            throw new ErrorHandle(err.status, err.message)
+            return res.status(err.status).send(error(err.message, err.status))
         }
     }
 
-    async increaseVisits(value: number): Promise<CountApi> {
+    async increaseVisits(req: Request, res: Response): Promise<CountApi> {
         try {
-            if(!value) {
+            const { value } = req.body
+
+            if (!value) {
                 throw new ErrorHandle(400, 'Value is mandatory!')
             }
 
             const result = await countapi.update(process.env.TON_SITE, process.env.COUNT_API_KEY, value).then((result) => {
                 return result
             })
-            return result
+
+            return res.status(200).send(success('SUCCESS', { data: result }, 200))
 
         } catch (err) {
-            throw new ErrorHandle(err.status, err.message)
+            return res.status(err.status).send(error(err.message, err.status))
         }
     }
 }
